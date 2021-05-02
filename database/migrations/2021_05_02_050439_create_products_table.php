39162fe1a30ce58de568c2a6813ddf4860a52558
Kishor Laskar <30683532+kishorlaskar@users.noreply.github.com>
2021-05-02 12:00:24 +0600

    create migration files and Databse

--- database/migrations/2021_05_02_050439_create_products_table.php
+++ database/migrations/2021_05_02_050439_create_products_table.php
@@ -14,7 +14,14 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
+            $table->string('product_img')->nullable();
+//            $table->integer('user_id')->unsigned()->index();
             $table->timestamps();
         });
     }
